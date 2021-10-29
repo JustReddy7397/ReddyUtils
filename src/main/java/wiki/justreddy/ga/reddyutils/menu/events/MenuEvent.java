46e4ef050a144f6a8f7238c7e04187710b7a81c0
JustReddy <j.dulk@leerling.leokanner.nl>
2021-10-29 20:44:58 +0200

    Deleted the anvil menu and the anvilmenuevent

--- src/main/java/wiki/justreddy/ga/reddyutils/menu/events/MenuEvent.java
+++ src/main/java/wiki/justreddy/ga/reddyutils/menu/events/MenuEvent.java
@@ -3,16 +3,16 @@ package wiki.justreddy.ga.reddyutils.menu.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.PrepareAnvilEvent;
 import org.bukkit.inventory.InventoryHolder;
-import wiki.justreddy.ga.reddyutils.menu.AnvilMenu;
 import wiki.justreddy.ga.reddyutils.menu.ChestMenu;
 import wiki.justreddy.ga.reddyutils.menu.HopperMenu;
 
 public class MenuEvent implements Listener {
 
     @EventHandler
-    public void onMenuClick(InventoryClickEvent e){
+    public void onMenuClick(InventoryClickEvent e) {
+
+
         InventoryHolder holder = e.getInventory().getHolder();
 
         if (holder instanceof ChestMenu) {
@@ -22,29 +22,19 @@ public class MenuEvent implements Listener {
             }
             ChestMenu chestMenu = (ChestMenu) holder;
             chestMenu.handleMenu(e);
-        }else if(holder instanceof HopperMenu){
+        } else if (holder instanceof HopperMenu) {
             e.setCancelled(true);
-            if(e.getCurrentItem() == null){
+            if (e.getCurrentItem() == null) {
                 return;
             }
             HopperMenu hopperMenu = (HopperMenu) holder;
             hopperMenu.handleMenu(e);
-        }else if(holder instanceof AnvilMenu){
-            e.setCancelled(true);
-        }
 
+
+        }
     }
 
-    @EventHandler
-    public void onAnvilRename(PrepareAnvilEvent e){
-        InventoryHolder holder = e.getInventory().getHolder();
 
-        if(holder instanceof AnvilMenu){
-            AnvilMenu anvilMenu = (AnvilMenu) holder;
-            anvilMenu.handleMenu(e);
-        }
-
-    }
 
 
 }
