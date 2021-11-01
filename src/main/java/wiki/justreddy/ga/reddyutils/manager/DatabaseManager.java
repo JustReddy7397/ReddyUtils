2078d715ad6dce10aeff61a3a4441d244d59d9cf
JustReddy <j.dulk@leerling.leokanner.nl>
2021-11-01 16:48:00 +0100

    Updated XSeries and added database manager i guess???

---
+++ src/main/java/wiki/justreddy/ga/reddyutils/manager/DatabaseManager.java
@@ -0,0 +1,77 @@
+package wiki.justreddy.ga.reddyutils.manager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.*;
+
+public class DatabaseManager {
+
+    private Connection con;
+
+    public void connectMysQL(String database, String username, String password, String host, int port) {
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + database, username, password);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void connectH2(JavaPlugin plugin, String storagePath) {
+        try {
+            Class.forName("org.h2.Driver");
+            con = DriverManager.getConnection("jdbc:h2:" + plugin.getDataFolder().getAbsolutePath() + "/" + storagePath);
+        } catch (SQLException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        if (isConnected()) {
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private boolean isConnected() {
+        return con != null;
+    }
+
+
+    public void update(String qry) {
+        try {
+            getConnection().createStatement().executeUpdate(qry);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+    public ResultSet getResult(String qry) {
+        try {
+            return getConnection().createStatement().executeQuery(qry);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public PreparedStatement prepareStatement(String qry) {
+
+        try{
+            getConnection().prepareStatement(qry);
+        }catch (SQLException ex){
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+
+    public Connection getConnection() {
+        return con;
+    }
+}
